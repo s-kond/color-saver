bd7bd14ccd272229d52aa3dfa8a58508cdc100f3
Stefan Kondring <kondring.s@gmx.de>
2022-09-30 16:12:48 +0200

    add form to create new cards

---
+++ src/components/Form/Create.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import "./Create.css"
+
+export default function Create({onHandleSubmit}) {
+    const [color, setColor] = useState("")
+    
+    function handleSetColor(chosenColor){
+        setColor(chosenColor)
+        console.log(color);
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const { colorTextInput } = form.elements;
+        onHandleSubmit(colorTextInput.value);
+      }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h2>New Card</h2>
+            <input className="colorPicker" name="colorPicker" id="colorPicker" type="color" value={color} onChange={(event)=>handleSetColor(event.target.value)}/>
+            <input className="colorTextInput" name="colorTextInput" type="text" value={color} onInput={(event)=>handleSetColor(event.target.value)} maxLength="7" />
+            <button className="addColorButton" type="submit">Add</button>
+        </form>
+    )
+}
