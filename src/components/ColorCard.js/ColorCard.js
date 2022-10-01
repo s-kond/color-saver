80bb1522832cd409a84fa651eb67f8efb1b914ff
Stefan Kondring <kondring.s@gmx.de>
2022-10-01 17:34:18 +0200

    add "copied!"-tag

--- src/components/ColorCard.js/ColorCard.js
+++ src/components/ColorCard.js/ColorCard.js
@@ -1,19 +1,31 @@
 import "./ColorCard.css"
 
 export function ColorCard({colors, onHandleDelete, onHandleEdit}) {
-    function copyCode(input){
+    function copyCode(event, input){
         try {
             console.log(input);
             navigator.clipboard.writeText(input);
+            showCopiedTag(event);
          } catch (error) {
              console.log(error.message)
          }
     }
-    
+
+    //Funktion von MDN adaptiert: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Tips
+    function showCopiedTag(event) {
+        //hiermit wird das p-element "copied!" angesteuert:
+        event.target.children[3].classList = "copied-message";
+        requestAnimationFrame((time) => {
+          requestAnimationFrame((time) => {
+            event.target.children[3].classList = "copied-message hidden";
+          });
+        });
+      }
+
     return (
         colors.map((entry) => {
             return (
-                <button className='color-container__card' onClick={()=> copyCode(entry.color)} style={{backgroundColor: entry.color}}>
+                <button className='color-container__card' onClick={(event)=> copyCode(event, entry.color)} style={{backgroundColor: entry.color}}>
                     <p className="colorName">{entry.name}</p>
                     <input type="text" className="colorText-input" 
                         value={entry.color} onInput={(event)=>onHandleEdit(event, entry.id, event.target.value)} 
@@ -21,6 +33,7 @@ export function ColorCard({colors, onHandleDelete, onHandleEdit}) {
                     <button type="button" className="delete-button" 
                         onClick={(event)=>onHandleDelete(event, entry.id)}
                     >X</button>
+                    <p className="copied-message hidden">copied!</p>
                 </button>
             )
         })
